a99290aed862ceb4680e9de28be97d8f8d6187e3
Syntax-Virtuoso <shashvatkr11@gmail.com>
2024-11-22 00:56:52 +0530

    Made home page of site

---
+++ website/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./about/*.html", "./contacct/*.html", "./documentation/*.html", "./download/*.html", "./features/*.html", "./updates/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        "sm":'1px'
+      }
+    },
+  },
+  plugins: [],
+}
+
